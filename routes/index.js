d67766dc70797032a36d458a9e55a6e11bf55823
Dengfeng Liu <liudf0716@gmail.com>
2021-03-19 18:09:45 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,53 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* vpp interface operation */
-router.get('if_op', function(req, res, next) {
-	v_ifplugin.if_op(req, res, next);
+// reference doc
+// https://docs.ligato.io/en/latest/user-guide/reference/
+// 
+
+/* 
+ * vpp wan operation 
+ */
+router.all('/config/interfaces/wan', function(req, res, next) {
+	v_ifplugin.wan_op(req, res, next);
 });
 
+/*
+ * vpp lan operation
+ */
+router.all('/config/interfaces/lan', function(req, res, next) {
+	v_ifplugin.lan_op(req, res, next);
+});
+
+/*
+ * vpp default route
+ */
+router.all('/config/route/gw', function(req, res, next) {
+	v_l3plugin.gw_op(req, res, next);
+});
+
+/*
+ *
+ */
+router.all('/config/route/add', function(req, res, next) {
+	v_l3plugin.route_add_op(req, res, next);
+});
+
+/*
+ *
+ */
+router.all('/config/route/del', function(req, res, next) {
+	v_l3plugin.route_del_op(req, res, next);
+});
+
+
 /* vpp snat operation */
-router.get('snat_op', function(req, res, next) {
+router.all('/config/', function(req, res, next) {
 	v_natplugin.snat_op(req, res, next);
 });
 
 /* vpp dnat operation */
-router.get('dnat_op', function(req, res, next) {
+router.all('/dnat_op', function(req, res, next) {
 	v_natplugin.dnat_op(req, res, next);
 });
 
