43cad2d70f69c7974daa069ab63ea7ec43e42f54
Dengfeng Liu <liudf0716@gmail.com>
2021-03-19 18:16:43 +0800

    Update natplugin.js

--- plugins/vpp/natplugin/natplugin.js
+++ plugins/vpp/natplugin/natplugin.js
@@ -1,9 +1,26 @@
 const { Etcd3 } = require('etcd3');
 const client = new Etcd3(options = { hosts: '127.0.0.1:2379' });
 
+// https://docs.ligato.io/en/latest/user-guide/reference/
+// https://github.com/ligato/vpp-agent/blob/master/proto/ligato/vpp/nat/nat.proto
+
+/*
+ * vpp snat operation
+ * key:
+ * /vnf-agent/vpp1/config/vpp/nat/v2/nat44-global/<settings>
+ * value:
+ * 
+ */
 function snat_op(req, res, next) {
 }
 
+/*
+ * vpp dnat operation
+ * key:
+ * /vnf-agent/vpp1/config/vpp/nat/v2/dnat44/<label>
+ * value:
+ * 
+ */
 function dnat_op(req, res, next) {
 }
 
